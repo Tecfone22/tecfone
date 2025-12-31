3148459101f20ba5f2293f79eb8fe84c44eec1af
Tecfone22 <tecfone22@hotmail.com>
2025-12-31 10:43:38 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
