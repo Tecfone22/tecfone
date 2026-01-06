e56c6fcb524784e8d4065bbbe420d50d671378ef
Tecfone22 <tecfone22@hotmail.com>
2026-01-06 09:12:06 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { 
   LayoutDashboard, Package, ShoppingCart, Wrench, Sparkles, Smartphone 
-} from 'lucide-center';
+} from 'lucide-react';
 
 import Dashboard from './components/Dashboard';
 import Inventory from './components/Inventory';
